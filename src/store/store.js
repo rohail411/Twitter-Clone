63013373ab9cfaa8f7411066d74abc974bfc9266
rohail411 <47238303+rohail411@users.noreply.github.com>
2019-12-03 00:11:54 +0500

    sotre
    Updated

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { createStore,applyMiddleware,combineReducers,compose } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import userReducer from './reducers/userReducer';
@@ -7,20 +7,17 @@ import uiReducer from './reducers/uiReducer';
 
 const initialState = {};
 
-const middleware = [thunk];
+const middleware = [ thunk ];
+
+const composeEnhancer =
+	process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const reducers = combineReducers({
-    user:userReducer,
-    data:dataReducer,
-    ui:uiReducer
+	user: userReducer,
+	data: dataReducer,
+	ui: uiReducer
 });
 
-const store = createStore(
-    reducers,
-    initialState,
-    compose(applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-);
+const store = createStore(reducers, initialState, composeEnhancer(applyMiddleware(...middleware)));
 
 export default store;
-
